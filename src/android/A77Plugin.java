4aea1d4b4769cc041d699161f0f461cc9e940092
staspl <stanislav.ploschansky@outsystems.fi>
2021-07-21 11:11:30 +0300

    add toast

--- src/android/A77Plugin.java
+++ src/android/A77Plugin.java
@@ -9,6 +9,46 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 
+// {{ ------------- Toast test -------------------------
+import android.widget.Toast;
+// Cordova-required packages
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ToastyPlugin extends CordovaPlugin {
+  private static final String DURATION_LONG = "long";
+  @Override
+  public boolean execute(String action, JSONArray args,
+    final CallbackContext callbackContext) {
+      // Verify that the user sent a 'show' action
+      if (!action.equals("show")) {
+        callbackContext.error("\"" + action + "\" is not a recognized action.");
+        return false;
+      }
+      String message;
+      String duration;
+      try {
+        JSONObject options = args.getJSONObject(0);
+        message = options.getString("message");
+        duration = options.getString("duration");
+      } catch (JSONException e) {
+        callbackContext.error("Error encountered: " + e.getMessage());
+        return false;
+      }
+      // Create the toast
+      Toast toast = Toast.makeText(cordova.getActivity(), message,
+        DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
+      // Display toast
+      toast.show();
+      // Send a positive result to the callbackContext
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+      callbackContext.sendPluginResult(pluginResult);
+      return true;
+  }
+}
+
+// }} ------------- Toast test -------------------------
+
 public class A77Plugin extends CordovaPlugin {
 
     @Override
--- www/A77Plugin.js
+++ www/A77Plugin.js
@@ -3,6 +3,12 @@ var exec = require('cordova/exec');
 module.exports = {
     get: function(success, error, options) {
 	success ( "Aaaaaaaaaaa" );
+
+	var options = {};
+	options.message = "TEST msg";
+	options.duration = "long";
+	cordova.exec(success, error, 'ToastyPlugin', 'show', [options]);
+
         // exec(success, error, "IMEI", "get", [options]);
     }
 };
